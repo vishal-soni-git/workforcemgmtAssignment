ebb94732b7974437dbe705b5120bacdf085f7f1c
Vishal Soni <vso55089@gmail.com>
2025-08-03 17:24:53 +0530

    Add all Endpoints of features.

--- src/main/java/com/railse/workforcemgmt/controller/TaskManagementController.java
+++ src/main/java/com/railse/workforcemgmt/controller/TaskManagementController.java
@@ -2,11 +2,14 @@ package com.railse.workforcemgmt.controller;
 
 import java.util.List;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.railse.workforcemgmt.dto.AssignByReferenceRequest;
@@ -16,6 +19,7 @@ import com.railse.workforcemgmt.dto.TaskFetchByDateRequest;
 import com.railse.workforcemgmt.dto.TaskManagementDto;
 import com.railse.workforcemgmt.dto.UpdateTaskRequest;
 import com.railse.workforcemgmt.model.TaskComment;
+import com.railse.workforcemgmt.model.enums.Priority;
 import com.railse.workforcemgmt.model.response.Response;
 import com.railse.workforcemgmt.service.TaskManagementService;
 
@@ -47,7 +51,7 @@ public class TaskManagementController {
         return taskManagementService.updateTasks(request);
     }
 
-    // Assign task(s) by reference
+    // Assign task(s) by reference bug #1
     @PostMapping("/assign-by-ref")
     public String assignByReference(@RequestBody AssignByReferenceRequest request) {
         return taskManagementService.assignByReference(request);
@@ -59,7 +63,25 @@ public class TaskManagementController {
         return taskManagementService.fetchTasksByDate(request);
     }
 
+    // Feature #1
+     @GetMapping("/priority")
+    public ResponseEntity<List<TaskManagementDto>> getTasksByPriority(
+            @RequestParam("priority") Priority priority) {
+        List<TaskManagementDto> tasks = taskManagementService.fetchTasksByPriority(priority);
+        return ResponseEntity.ok(tasks);
+    }
 
+    //Feature #2
+      @PutMapping("/{taskId}/priority")
+    public ResponseEntity<TaskManagementDto> updateTaskPriority(
+        @PathVariable Long taskId,
+        @RequestParam("priority") Priority priority) {
+       TaskManagementDto updatedTask = taskManagementService.updateTaskPriority(taskId, priority);
+       return ResponseEntity.ok(updatedTask);
+}
+
+
+    //Feature #3
      @PostMapping("/comments")
     public Response<String> addComments(@RequestBody TaskCommentRequest request) {
          taskManagementService.addComment(request.getTaskId(),request.getUserId(),request.getComment());
